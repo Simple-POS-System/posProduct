26466b9fde052b298519e4bf94d747ae4ccc2a3d
diniranga <dinirangapremanayake@gmail.com>
2023-11-01 21:56:21 +0530

    initial project file

---
+++ src/main/java/com/ead/posproduct/PosProductApplication.java
@@ -0,0 +1,13 @@
+package com.ead.posproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosProductApplication.class, args);
+    }
+
+}
