b1bdf8b269d68f367456a00aff9aedea253f8dfe
diniranga <dinirangapremanayake@gmail.com>
2023-11-02 10:11:31 +0530

    updated delete function

--- src/main/java/com/ead/posproduct/ProductRepository.java
+++ src/main/java/com/ead/posproduct/ProductRepository.java
@@ -8,4 +8,5 @@ public interface ProductRepository extends MongoRepository<Product,String> {
    Product findFirstByOrderByProductIdDesc();
    Product findProductByProductName(String productName);
    Product findProductByProductId(String productId);
+   Product deleteProductByProductId(String productId);
 }
